4b4682cff117ffa46ef274145a9e3e7b2504b9ae
Jeffrey Westerkamp <jeffrey@pixelindustries.com>
2017-07-04 19:02:43 +0200

    [Feature] Initial version

---
+++ index.js
@@ -0,0 +1,5 @@
+const yaml = require('js-yaml');
+const fs   = require('fs');
+const path = require('path');
+
+module.exports = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '.eslintrc.yml'), 'utf8'));
